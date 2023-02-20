ab742fa173d0b2822d781ba15f169222d3e1297c
Ismael <ismael.bortoluzzi77@gmail.com>
2023-02-20 11:37:30 -0300

    using generic class for the other view

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from tag.models import Tag
 from rest_framework.views import APIView
@@ -21,37 +21,10 @@ class RecipeAPIV2List(ListCreateAPIView):
     pagination_class = RecipeAPIV2Pagination
 
 
-class RecipeAPIV2Detail(APIView):
-    def get_object(self):
-        recipe = get_object_or_404(
-            Recipe.objects.get_published(),
-            pk=self.kwargs.get('pk')
-        )
-        return recipe
-
-    def get(self, request, pk):
-        serializer = RecipeSerializer(
-            instance=self.get_object(),
-            many=False,
-            context={'request': request},
-        )
-        return Response(serializer.data)
-
-    def patch(self, request, pk):
-        serializer = RecipeSerializer(
-            instance=self.get_object(),
-            data=request.data,
-            many=False,
-            context={'request': request},
-            partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        self.get_object().delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class RecipeAPIV2Detail(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIV2Pagination
 
 
 @api_view()
