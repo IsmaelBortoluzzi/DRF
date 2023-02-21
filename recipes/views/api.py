806c61afd241d9ba83efbe134543aed6bd35ab66
Ismael Bortoluzzi <ti4@ballke.com.br>
2023-02-21 15:45:50 -0300

    adicionando permissions

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from tag.models import Tag
 from rest_framework.views import APIView
@@ -9,6 +10,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
+from ..permissions import IsOwner
 
 
 class RecipeAPIV2Pagination(PageNumberPagination):
@@ -18,12 +20,13 @@ class RecipeAPIV2Pagination(PageNumberPagination):
 class RecipeAPIV2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIV2Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
     def get_queryset(self):
         qs = Recipe.objects.get_published()
         category_id = self.request.query_params.get('category_id', '')
 
-        if category_id != '' and category_id.is_numeric():
+        if category_id != '' and category_id.isnumeric():
             qs = qs.filter(category_id=category_id)
 
         return qs
@@ -33,8 +36,18 @@ class RecipeAPIV2ViewSet(ModelViewSet):
             return Response(status=status.HTTP_404_NOT_FOUND)
         return super().list(request, *args, **kwargs)
 
+    def get_permissions(self):
+        if self.request.method in ('DELETE', 'PATCH'):
+            return [IsOwner(), ]
+        return super().get_permissions()
+
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset(), pk=self.kwargs.get('pk'))
+        self.check_object_permissions(self.request, obj)
+        return obj
+
     def partial_update(self, request, *args, **kwargs):
-        obj = self.get_queryset().filter(pk=kwargs.get('pk')).first()
+        obj = self.get_object()
         serializer = RecipeSerializer(
             instance=obj,
             data=request.data,
